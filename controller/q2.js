ecf9c9bb5f535244afae28a5bd87428801ae1aab
pcezar1118 <pcezar1118@gmail.com>
2022-11-11 18:51:54 -0200

    soma

---
+++ controller/q2.js
@@ -0,0 +1,31 @@
+function clicar() {
+    let = document.querySelector("#n1").value;
+    let = document.querySelector("#n2").value;
+    let resultado = parseInt(n1) + parseInt(n2);
+    document.querySelector("#resultado").innerHTML = resultado;
+}
+console.log(resultado);
+
+
+
+
+
+/*class Numeros {
+constructor(numero) {
+    this.numero = numero;
+    }
+    soma() {
+        return this.numero + ' ' + this.numero;
+    }
+
+}
+
+function resultado() {
+let n1 = new Numeros(parseInt(x, 10));
+document.querySelector("#n1");
+let n2 = new Numeros(parseInt(y, 10));
+document.querySelector("#n2");
+    return soma();
+}
+console.log(soma());
+*/
